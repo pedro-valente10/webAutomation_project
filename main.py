23a3281763bdc8ee786ec87e41cdb48e71be8a65
Pedro Valente <pedrohenriquepvalente@gmail.com>
2026-05-01 19:38:07 -0300

    conclusao

--- main.py
+++ main.py
@@ -3,18 +3,48 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
 
-options = Options()
+import os
+import base64
+from email.mime.text import MIMEText
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
-caminho_perfil = r"C:\Users\Pedro Valente\AppData\Local\Google\Chrome\User Data"
-options.add_argument(f"user-data-dir={caminho_perfil}")
-options.add_argument("--profile-directory=Default")
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
 
-options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument("--no-first-run")
-options.add_argument("--no-service-autorun")
-options.add_argument("--password-store=basic")
+def autenticar():
+    creds = None
 
-navegador = webdriver.Chrome(options=options)
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return creds
+
+def criar_email(remetente, destinatario, assunto, mensagem):
+    msg = MIMEText(mensagem)
+    msg['to'] = destinatario
+    msg['from'] = remetente
+    msg['subject'] = assunto
+
+    raw = base64.urlsafe_b64encode(msg.as_bytes())
+    return {'raw': raw.decode()}
+
+def enviar_email(service, mensagem):
+    return service.users().messages().send(userId="me", body=mensagem).execute()
+
+#busca usando o selenium
+navegador = webdriver.Chrome()
 time.sleep(2)
 navegador.get("https://www.amazon.com.br/")
 navegador.maximize_window()
@@ -25,7 +55,7 @@ barra_pesquisa.send_keys("Galaxy S25")
 barra_pesquisa.send_keys(Keys.ENTER)
 time.sleep(2)
 
-# Localiza o container do produto pelo ASIN e clica no link
+#localiza o container do produto pelo ASIN e clica no link
 produto1 = navegador.find_element("css selector", f'div[data-asin="B0DSY665M3"] a.a-link-normal')
 navegador.execute_script("arguments[0].scrollIntoView({block: 'center'})", produto1)
 produto1.click()
@@ -37,18 +67,18 @@ precoFloat_produto1 = navegador.find_element("class name", "a-price-fraction").t
 print(f"Nome - {nome_produto1}")
 print(f"Preço - R$ {precoInt_produto1},{precoFloat_produto1}")
 
-#cria e muda para a nova guia do email
-navegador.execute_script("window.open('');")
-abas = navegador.window_handles
-navegador.switch_to.window(abas[1])
-navegador.get("https://mail.google.com/mail/u/0/#inbox")
+#cria e manda o email
+creds = autenticar()
+service = build('gmail', 'v1', credentials=creds)
 
-botao_escrever = navegador.find_element("css selector", "T-I.T-I-KE.L3")
-botao_escrever.click()
-botao_destinatario = navegador.find_element("css selector", "agP.aFw")
-botao_destinatario.click()
-navegador.send_keys("pedrohenriquepvalente@gmail.com")
-navegador.send_keys(Keys.ENTER)
+email = criar_email(
+    "me",
+    "pedrohenriquepvalente@gmail.com",
+    "Preço do produto 1",
+    f"Nome: {nome_produto1}\nPreço: R$ {precoInt_produto1},{precoFloat_produto1}"
+)
 
-time.sleep(10)
+enviar_email(service, email)
+
+time.sleep(5)
 navegador.quit()
